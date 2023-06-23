34ee5e4e6d50ce0ed276015bd0487e54fb0d0ddf
Omar Barra <78673626+Baraski-jr@users.noreply.github.com>
2023-06-23 21:09:05 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('form')
+const emailToTest = document.querySelector('.email-box');
+const errorMessage = document.querySelector('.erro-mesg');
+const erroIcon = document.querySelector('.denger-icon');
+
+
+const Validation = (emailToTest) => {
+
+}
